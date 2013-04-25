b65fb13b9da0ac4063fd64c6b2c68709fb82d4bf
Jeet Sukumaran <jeetsukumaran@gmail.com>
2013-04-25 13:45:47 -0400

    test actually tests

--- test/src/newick_writer_basic.cpp
+++ test/src/newick_writer_basic.cpp
@@ -1,4 +1,4 @@
-#include <platypus/platypus.hpp>
+#include <sstream>
 #include "tests.hpp"
 
 int main () {
@@ -13,6 +13,24 @@ int main () {
     newick_writer.set_tree_is_rooted_getter([](const TreeType & tree)->bool {return tree.is_rooted();} );
     newick_writer.set_node_label_getter([](const TreeType::value_type & nv)->std::string {return nv.get_label();} );
     newick_writer.set_edge_length_getter([](const TreeType::value_type & nv)->double {return nv.get_edge_length();} );
-    newick_writer.write(trees.cbegin(), trees.cend(), std::cout);
+
+    std::ostringstream o1;
+    newick_writer.write(trees.cbegin(), trees.cend(), o1);
+
+    std::vector<TreeType> trees2;
+    auto tree_reader2 = get_test_data_tree_newick_reader(trees2);
+    std::string tree_string2 = o1.str();
+    tree_reader2.read_from_string(tree_string2, "newick");
+    std::ostringstream o2;
+    newick_writer.write(trees2.cbegin(), trees2.cend(), o2);
+    std::string tree_string3 = o2.str();
+    if (tree_string2 != tree_string2) {
+        fail_test(__FILE__,
+                tree_string2,
+                tree_string3);
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
